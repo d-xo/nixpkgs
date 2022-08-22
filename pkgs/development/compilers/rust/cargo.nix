0ab727378a3bd36a919d53add8d9b6496fc0995b
David Terry <me@xwvvvvwx.com>
2022-08-22 16:35:05 +0200

    make cargo nightly

--- pkgs/development/compilers/rust/cargo.nix
+++ pkgs/development/compilers/rust/cargo.nix
@@ -31,6 +31,10 @@ rustPlatform.buildRustPackage {
   # fixes: the cargo feature `edition` requires a nightly version of Cargo, but this is the `stable` channel
   RUSTC_BOOTSTRAP = 1;
 
+  configureFlags = [
+    "--release-channel=nightly"
+  ];
+
   postInstall = ''
     # NOTE: We override the `http.cainfo` option usually specified in
     # `.cargo/config`. This is an issue when users want to specify
